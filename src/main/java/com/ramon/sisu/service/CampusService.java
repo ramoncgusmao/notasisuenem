66fdf9324d6f96eaa0dd8ad737dfe74798b3693a
ramon cantanhede gusmão <ramoncgusmao@gmail.com>
2020-01-14 18:43:30 -0300

    adicionando mensagem de erro

--- src/main/java/com/ramon/sisu/service/CampusService.java
+++ src/main/java/com/ramon/sisu/service/CampusService.java
@@ -3,12 +3,14 @@ package com.ramon.sisu.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ramon.sisu.domain.model.Campus;
 import com.ramon.sisu.domain.model.Campus;
 import com.ramon.sisu.repository.CampusRepository;
+import com.ramon.sisu.service.exception.DataIntegrityException;
 import com.ramon.sisu.service.exception.ObjectNotFoundException;
 
 @Service
@@ -21,8 +23,13 @@ public class CampusService {
 	private FaculdadeService faculdadeService;
 	public Campus criarCampus(Campus campus) {
 		
-		carregarFaculdade(campus);
-		return repository.save(campus);
+		try {
+			carregarFaculdade(campus);
+			return repository.save(campus);
+		} catch (DataException e) {
+			throw new DataIntegrityException("erro ao salvar: " + e.getMessage());
+		}
+		
 	}
 	private void carregarFaculdade(Campus campus) {
 		campus.setFaculdade(faculdadeService.findBySigla(campus.getFaculdade().getSigla()));
